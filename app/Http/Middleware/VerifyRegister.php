6f26c97ca7b3b8fdcc13328bcbfc6c14280e8a74
NeverBehave <luoxinhao77@gmail.com>
2017-09-02 17:07:21 +0800

    makes better

--- app/Http/Middleware/VerifyRegister.php
+++ app/Http/Middleware/VerifyRegister.php
@@ -17,7 +17,7 @@ class VerifyRegister
 	public function handle($request, Closure $next)
 	{
 		if (Config::KeyValue('is_able_to_register')->value == false) {
-			return Redirect('errors.custom')->with('message', '注册被停用');
+			return view('errors.custom')->with('message', '注册被停用');
 		}
 
 		return $next($request);
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.base')
+@extends('layouts.error')
 
 @section('title')
     404 No Found
