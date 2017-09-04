c34d610c313bd3de63cb6eae4cdfc602f09bd07e
NeverBehave <luoxinhao77@gmail.com>
2017-09-04 23:28:58 +0800

    fix and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-	    $this->call(ConfigSeeder::class);
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// $this->call(UsersTableSeeder::class);
+		$this->call(ConfigSeeder::class);
+		$this->call(ResourceSeeder::class);
     }
 }
---
+++ database/seeds/ResourceSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ResourceSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		//
+		$default = [
+			'code'        => 'money',
+			'name'        => 'money',
+			'description' => 'This is money !',
+			'type'        => 0,
+		];
+
+		\App\Config::query()->create($default);
+
+		$faker = new Faker();
+		for ($i = 1; $i <= 3; $i++) {
+			\App\Config::query()->create([
+				'code'        => str_random(5),
+				'name'        => $faker->name,
+				'description' => $faker->words,
+				'type'        => $faker->numberBetween(0,3),
+			]);
+		}
+	}
+}
