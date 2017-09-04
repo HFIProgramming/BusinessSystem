4c336b19c2471ee0e58e5fcee89404b3abc1dd40
NeverBehave <luoxinhao77@gmail.com>
2017-09-04 23:20:38 +0800

    fix scpoe

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -69,7 +69,7 @@ class TransactionController extends Controller
 		if (empty($sellerItem = $seller->resources()->id($request->resource_id))) {
 			return view('errors.custom')->with('message', '对方：交易物品不存在');
 		}
-		if (!($buyer->type - $seller->type == 1 && Resources::id($sellerItem->resource_id)->type - $seller->type == 1)->first()) {
+		if (!($buyer->type - $seller->type == 1 && Resources::id($sellerItem->resource_id)->first()->type - $seller->type == 1)) {
 			return view('errors.custom')->with('message', '你们之间不能交易这两种物品');
 		}
 		event(new NewTransaction($seller, $buyer, $sellerItem, $buyerItem, $seller_amount, $buyer_amount, $type));
