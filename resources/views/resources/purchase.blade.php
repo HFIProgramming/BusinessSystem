b65aa01b6bd797eb3f0e8cb41810b28494337ce2
NeverBehave <luoxinhao77@gmail.com>
2017-09-04 23:57:17 +0800

    fix purchase

--- resources/views/resources/purchase.blade.php
+++ resources/views/resources/purchase.blade.php
@@ -32,13 +32,13 @@
                         <div class="mdui-textfield mdui-textfield-floating-label">
                             <i class="mdui-icon material-icons adjust_mdui_icon">shopping_basket</i>
                             <label class="mdui-textfield-label">目标商品ID</label>
-                            <input class="mdui-textfield-input" id="item_id" type="number" required/>
+                            <input class="mdui-textfield-input" id="item_id" name="item_id" type="number" required/>
                             <div class="mdui-textfield-error">目标商品不能为空</div>
                         </div>
                         <div class="mdui-textfield mdui-textfield-floating-label ">
                             <i class="mdui-icon material-icons adjust_mdui_icon">add</i>
                             <label class="mdui-textfield-label">购买数量</label>
-                            <input class="mdui-textfield-input" id="amount" type="number" required/>
+                            <input class="mdui-textfield-input" id="amount" name="amount" type="number" required/>
                             <div class="mdui-textfield-error">购买数量不能为空</div>
                         </div>
                         <button data-no-instant class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme mdui-center">提交</button>
