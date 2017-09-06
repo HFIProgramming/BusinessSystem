d68879396e8cb92aba2a8286084ab062e1620c50
NeverBehave <luoxinhao77@gmail.com>
2017-09-06 23:26:08 +0800

    add Transaction Rule

--- app/User.php
+++ app/User.php
@@ -7,57 +7,62 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+	use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'type',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password', 'type',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
 
-    public function resources()
-    {
-        return $this->hasMany(UserResource::class);
-    }
+	public function resources()
+	{
+		return $this->hasMany(UserResource::class);
+	}
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function outcomeTransaction()
-    {
-        return $this->hasMany(Transaction::class, 'seller_id');
-    }
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function outcomeTransaction()
+	{
+		return $this->hasMany(Transaction::class, 'seller_id');
+	}
 
-    public function incomeTransaction()
-    {
-        return $this->hasMany(Transaction::class, 'buyer_id');
-    }
+	public function incomeTransaction()
+	{
+		return $this->hasMany(Transaction::class, 'buyer_id');
+	}
 
-    public function scopeId($query, $id)
-    {
-        return $query->where('id', $id);
-    }
+	public function scopeId($query, $id)
+	{
+		return $query->where('id', $id);
+	}
 
-    public function scopeType($query, $type)
-    {
-        return $query->where('type', $type);
-    }
+	public function scopeType($query, $type)
+	{
+		return $query->where('type', $type);
+	}
 
-    public function getAllTransAttribute()
-    {
-        return $this->incomeTransaction()->get()->merge($this->outcomeTransaction()->get());
-    }
+	public function getAllTransAttribute()
+	{
+		return $this->incomeTransaction()->get()->merge($this->outcomeTransaction()->get());
+	}
+
+	public function transactionRule()
+	{
+		return $this->belongsTo(UserTransactionRule::class, 'user_type', 'type');
+	}
 
 }
