042f2649d99544816ffaba4b6744817fc51310d7
NeverBehave <luoxinhao77@gmail.com>
2017-09-06 23:14:02 +0800

    fix trans

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -129,7 +129,7 @@
                                 </ul>
                             </li>
                         @else
-                            @foreach($user->AllTrans->sortByDesc('created_at')->first() as $trans)
+                            @foreach($user->AllTrans->sortByDesc('created_at') as $trans)
                                 <li class="mdui-collapse-item mdui-collapse-item-close">
                                     <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
                                         <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
