c9c6099a386b894c1a6e5dc6d23702ec70bbbaf0
ZihongLin <1046298030@qq.com>
2017-09-13 13:30:40 +0800

    route improve

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
 Route::group(['middleware' => 'register'], function () {
-	Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-	Route::post('register', 'Auth\RegisterController@register');
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
 });
 
 Route::get('error', 'HomeController@showErrorPage')->name('error');
@@ -34,49 +34,49 @@ Route::get('error', 'HomeController@showErrorPage')->name('error');
 
 Route::group(['middleware' => 'auth'], function () {
 
-	Route::get('/dashboard', 'UserController@index')->name('dashboard');
+    Route::get('/dashboard', 'UserController@index')->name('dashboard');
 
-	Route::group(['middleware' => 'suspend'], function () {
+    Route::group(['middleware' => 'suspend'], function () {
 
-		Route::group(['prefix' => 'transaction'], function () {
-			Route::get('/list', 'TransactionController@showTransactionList')->name('TransactionList');
-			Route::get('/income', 'TransactionController@showIncomeCreateForm')->name('TransIn');//买
-			Route::post('/income', 'TransactionController@buyFromUser')->name('doTransIn');
-			Route::get('/outcome', 'TransactionController@showOutcomeCreateForm')->name('TransOut');//卖
-			Route::post('/outcome', 'TransactionController@sellToUser')->name('doTransOut');
-			Route::get('/buygov', 'TransactionController@showBuyGovCreateForm')->name('BuyGov');//从政府买
-			Route::post('/buygov', 'TransactionController@buyFromGovernment')->name('doBuyGov');
-			Route::get('/sellgov', 'TransactionController@showSellGovCreateForm')->name('SellGov');//向政府卖
-			Route::post('/sellgov', 'TransactionController@sellToGovernment')->name('doSellGov');
-			Route::post('/confirm', 'TransactionController@handleTransaction')->name('confirmTrans');
-		});
-
-		Route::group(['prefix' => 'resource'], function () {
-			Route::get('/list', 'HomeController@showResource')->name('resource');
-			Route::get('/{id}', 'HomeController@showIndividualResource');
-			Route::get('/purchase', 'PurchaseController@showPurchaseForm')->name('purchaseForm');
-			Route::post('/purchase', 'PurchaseController@TopUp')->name('doPurchase');
-		});
-
-		Route::group(['prefix' => 'technology'], function () {
-		   Route::get('/show', 'TechnologyController@showTechPage')->name('showTech');
-		   Route::get('/update', 'TechnologyController@updateTech')->name('updateTech');
+        Route::group(['prefix' => 'transaction'], function () {
+            Route::get('/list', 'TransactionController@showTransactionList')->name('TransactionList');
+            Route::get('/income', 'TransactionController@showIncomeCreateForm')->name('TransIn');//买
+            Route::post('/income', 'TransactionController@buyFromUser')->name('doTransIn');
+            Route::get('/outcome', 'TransactionController@showOutcomeCreateForm')->name('TransOut');//卖
+            Route::post('/outcome', 'TransactionController@sellToUser')->name('doTransOut');
+            Route::get('/buygov', 'TransactionController@showBuyGovCreateForm')->name('BuyGov');//从政府买
+            Route::post('/buygov', 'TransactionController@buyFromGovernment')->name('doBuyGov');
+            Route::get('/sellgov', 'TransactionController@showSellGovCreateForm')->name('SellGov');//向政府卖
+            Route::post('/sellgov', 'TransactionController@sellToGovernment')->name('doSellGov');
+            Route::post('/confirm', 'TransactionController@handleTransaction')->name('confirmTrans');
         });
-	});
-	Route::group(['prefix' => 'announcement'], function () {
-		Route::get('/', 'HomeController@showAnnouncement')->name('announcement');
-	});
 
-	Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
-		Route::get('/', 'Admin\AdminController@showDashboard')->name('adminDashboard');
-		Route::get('/refreshUserResource', 'Admin\AdminController@refreshUserResource');
-		Route::get('/acquisition_price/list', 'Admin\ResourceController@listBots')->name('listBots');
-		Route::post('/acquisition_price/update', 'Admin\ResourceController@updateBots')->name('updateBots');
-		Route::get('/employment_price/list', 'Admin\ResourceController@listMiners')->name('listMiners');
-		Route::post('/employment_price/update', 'Admin\ResourceController@updateMiners')->name('updateMiners');
-		Route::get('/fiscal_year/show', 'Admin\RoundController@show')->name('showRound');
-		Route::post('/fiscal_year/change_total', 'Admin\RoundController@changeTotal')->name('changeTotalRound');
-		Route::post('/fiscal_year/change_current', 'Admin\RoundController@changeCurrent')->name('changeCurrentRound');
-	});
+        Route::group(['prefix' => 'resource'], function () {
+            Route::get('/list', 'HomeController@showResource')->name('resource');
+            Route::get('/{id}', 'HomeController@showIndividualResource');
+            Route::get('/purchase', 'PurchaseController@showPurchaseForm')->name('purchaseForm');
+            Route::post('/purchase', 'PurchaseController@TopUp')->name('doPurchase');
+        });
+
+        Route::group(['prefix' => 'technology'], function () {
+            Route::get('/show', 'TechnologyController@showTechPage')->name('showTech');
+            Route::get('/update', 'TechnologyController@updateTech')->name('updateTech');
+        });
+    });
+    Route::group(['prefix' => 'announcement'], function () {
+        Route::get('/', 'HomeController@showAnnouncement')->name('announcement');
+    });
+
+    Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin'], function () {
+        Route::get('/', 'AdminController@showDashboard')->name('adminDashboard');
+        Route::get('/refreshUserResource', 'AdminController@refreshUserResource');
+        Route::get('/acquisition_price/list', 'ResourceController@listBots')->name('listBots');
+        Route::post('/acquisition_price/update', 'ResourceController@updateBots')->name('updateBots');
+        Route::get('/employment_price/list', 'ResourceController@listMiners')->name('listMiners');
+        Route::post('/employment_price/update', 'ResourceController@updateMiners')->name('updateMiners');
+        Route::get('/fiscal_year/show', 'RoundController@show')->name('showRound');
+        Route::post('/fiscal_year/change_total', 'RoundController@changeTotal')->name('changeTotalRound');
+        Route::post('/fiscal_year/change_current', 'RoundController@changeCurrent')->name('changeCurrentRound');
+    });
 });
 
