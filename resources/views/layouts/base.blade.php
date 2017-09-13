ef0b3a9bec2b6bb66fbdfec3a9e131f1855be153
NeverBehave <luoxinhao77@gmail.com>
2017-09-13 17:11:38 +0800

    add admin panel

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -119,6 +119,10 @@
                 <a href="{{route('showTech')}}" class="mdui-list-item-content">Technology</a>
             </li>
         @endif
+        <li class="mdui-list-item mdui-ripple">
+            <i class="mdui-list-item-icon mdui-icon material-icons">dashboard</i>
+            <a href="{{route('announcement')}}" class="mdui-list-item-content">DashBoard</a>
+        </li>
         <li class="mdui-divider"></li>
         @if(auth()->id() == 1)
             <li class="mdui-list-item mdui-ripple">
