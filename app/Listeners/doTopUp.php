584089716e297311c8ca95e129c0f95043fdc4f1
ZihongLin <1046298030@qq.com>
2017-09-16 10:35:50 +0800

    try to fix purchase

--- app/Listeners/doTopUp.php
+++ app/Listeners/doTopUp.php
@@ -31,7 +31,7 @@ class doTopUp
 	public function handle(BuyStuff $event)
 	{
 		// First, check requirements
-		$userResources = $event->user->resources();
+		$userResources = $event->user->resources()->get();
 		$requirement = ($event->item->requirement)[$event->user->techLevel($event->item->required_tech)];
 
 		DB::beginTransaction();
