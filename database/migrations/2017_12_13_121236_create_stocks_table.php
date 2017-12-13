c5c1523e9e6dd491df93d68e68721c2793e4d94f
ZihongLin <1046298030@qq.com>
2017-12-13 14:42:15 +0800

    create company&stock table, update user table

--- database/migrations/2017_12_13_121236_create_stocks_table.php
+++ database/migrations/2017_12_13_121236_create_stocks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStocksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stocks', function (Blueprint $table) {
             $table->increments('id');
-            //id=1 => System
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('type');
-            //0=System 1=公司 2=投行
-            $table->rememberToken();
+            $table->float('current_price');
+            $table->string('history_prices');
+            $table->integer('company');
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stocks');
     }
 }
--- database/migrations/2017_12_13_121248_create_companies_table.php
+++ database/migrations/2017_12_13_121248_create_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
-            //id=1 => System
+            $table->integer('user_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('type');
-            //0=System 1=公司 2=投行
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
     }
 }
