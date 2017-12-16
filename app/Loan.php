43c957ad011bcca549b5ff8e6ae7e463ba8cee62
ZihongLin <1046298030@qq.com>
2017-12-16 17:22:25 +0800

    Loan<=>User Relations: Debtor & Creditor

--- app/Loan.php
+++ app/Loan.php
@@ -11,4 +11,14 @@ class Loan extends Model
     {
         return $this->belongsTo(Transaction::class, 'loan_transaction_id');
     }
+
+    public function creditor()
+    {
+        return $this->belongsTo(User::class, 'creditor_id');
+    }
+
+    public function debtor()
+    {
+        return $this->belongsTo(User::class, 'debtor_id');
+    }
 }
