09c3dbe76c5cafaa9cc4a181f76d733ff247cbdb
ZihongLin <1046298030@qq.com>
2017-12-18 01:06:03 +0800

    GenerateReports done

--- app/Listeners/GenerateReports.php
+++ app/Listeners/GenerateReports.php
@@ -3,6 +3,8 @@
 namespace App\Listeners;
 
 use App\Events\EndOfYear;
+use App\Loan;
+use App\Report;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -28,12 +30,42 @@ class GenerateReports
     {
         //
         $current = $event->current_year;
-        foreach(User::all() as $user)
+        $companyShareHolders = [];
+        foreach(User::type(2)->get() as $bank)
         {
-            if($user->type == 1 || $user->type == 2)
+            $components = [];
+            foreach($bank->resources as $userResource)
             {
-
+                if($userResource->resource->type == 3)//Stock
+                {
+                    $share = $userResource->amount / $userResource->resource->stock->total;
+                    if($share >= 0.1)
+                    {
+                        $components[$userResource->resource->stock->company->user_id] = $share;
+                        $companyShareHolders[$userResource->resource->stock->company->user_id][$bank->id] = $share;
+                    }
+                }
             }
+            $loan_total = Loan::where('creditor_id', $bank->id)->where('status', 'accepted')->get()->sum('amount');
+            Report::create([
+                'year' => $current,
+                'user_id' => $bank->id,
+                'type' => 'bank',
+                'components' => $components,
+                'loan_total' => $loan_total
+            ]);
+        }
+        foreach(User::type(1)->get() as $company)
+        {
+            $components = $companyShareHolders[$company->id];
+            Report::create([
+               'year' => $current,
+               'user_id' => $company->id,
+               'type' => 'company',
+               'stock_price' => $company->stock->current_price,
+               'profit' => $company->last_year_profit,
+               'components' => $components
+            ]);
         }
     }
 }
--- app/Report.php
+++ app/Report.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     //
+    protected $fillable = [
+        'year', 'user_id', 'type', 'components', 'stock_price', 'profit', 'loan_total'
+    ];
+
+    protected $casts = [
+        'components' => 'array'
+    ];
 }
