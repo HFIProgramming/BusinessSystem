6cf20a054925bc9e6ac81886256f4a7659b6536a
ZihongLin <1046298030@qq.com>
2017-12-17 17:18:44 +0800

    SaveToCompany uses Transactions

--- app/Listeners/SaveToCompany.php
+++ app/Listeners/SaveToCompany.php
@@ -5,7 +5,9 @@ namespace App\Listeners;
 use App\Company;
 use App\Config;
 use App\Events\EndOfYear;
+use App\Events\NewTransaction;
 use App\IntToVal;
+use App\User;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use function Sodium\compare;
@@ -33,6 +35,8 @@ class SaveToCompany
         //
         foreach(Company::all() as $company)
         {
+            $gains = [];
+            $gains[1] = $company->last_year_profit * (1 - $company->stock->dividend);//$$$$$$$$
             foreach($company->user->resources as $userResource)
             {
                 if($userResource->resource->type == 2)
@@ -51,19 +55,26 @@ class SaveToCompany
                         {
                             if($resource_id != 1)//Money is dealt with separately @TODO EXCLUDE Pollution Index
                             {
-                                //@TODO Maybe Stats
-                                $incomeUserResource = $company->user->resources()->resid($resource_id)->first();
-                                $increment = $amount * $quantity * $coeff;
-                                $incomeUserResource->amount += $increment;
-                                $incomeUserResource->save();
+                                if(!array_key_exists($resource_id, $gains))
+                                {
+                                    $gains[$resource_id] = 0;
+                                }
+                                $gains[$resource_id] += $amount * $quantity * $coeff;
                             }
                         }
                     }
                 }
             }
-            $money = $company->user->resources()->resid(1)->first();
-            $money->amount += $company->last_year_profit * (1 - $company->stock->dividend);
-            $money->save();
+            foreach($gains as $resource_id => $amount)
+            {
+                $seller = User::type(0)->first();
+                $sellerItem = $seller->resources()->resid($resource_id)->first();
+                $sellerAmount = round($amount);
+                $buyer = $company->user;
+                $buyerItem = $buyer->resources()->resid(1)->first();
+                $buyerAmount = 0;
+                event(new NewTransaction($seller, $seller, $buyer, $sellerItem, $buyerItem, $sellerAmount, $buyerAmount, 'yearly_yield'));
+            }
         }
     }
 }
