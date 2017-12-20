e3eb600f97e5f3f76c8b1beec1b14bc3e6290bae
ZihongLin <1046298030@qq.com>
2017-12-20 16:02:24 +0800

    finished IntToVal seeder

--- database/seeds/IntToValSeeder.php
+++ database/seeds/IntToValSeeder.php
@@ -51,7 +51,7 @@ class IntToValSeeder extends Seeder
             'value' => 1.2
         ]);
 
-        $arr = [[1.3, 199, 1000000], [1.2, 100, 200], [1.1, 50, 100], [1, -50, 50], [0.9, -100, -50], [0.8, -200, -100], [0.7, -1000000, -200]];
+        $arr = [[1.3, 200, 1000000], [1.2, 100, 200], [1.1, 50, 100], [1, -50, 50], [0.9, -100, -50], [0.8, -200, -100], [0.7, -1000000, -200]];
         foreach ($arr as $data)
         {
             IntToVal::create([
@@ -67,5 +67,24 @@ class IntToValSeeder extends Seeder
                 'value' => $data[0]
             ]);
         }
+
+        IntToVal::create([
+            'lower' => -1000000,
+            'upper' => 180,
+            'flag' => 'pollution_tax',
+            'value' => 0
+        ]);
+        IntToVal::create([
+            'lower' => 180,
+            'upper' => 250,
+            'flag' => 'pollution_tax',
+            'value' => 0.25
+        ]);
+        IntToVal::create([
+            'lower' => 250,
+            'upper' => 1000000,
+            'flag' => 'pollution_tax',
+            'value' => 0.5
+        ]);
     }
 }
