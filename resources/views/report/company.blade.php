9c05d8a27c9c9028ff87a808b1f3d783727856a7
JohannaSun <sunyx.johanna2016@outlook.com>
2017-12-20 21:10:11 +0800

    add some js in company report

--- resources/views/report/company.blade.php
+++ resources/views/report/company.blade.php
@@ -7,6 +7,56 @@
 @section('script')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.bundle.js"></script>
     <script>
+        var receivedInfo = [{
+            company_id: "1",
+            company_name: "scriTrashPeople",
+            year: "2001",
+            price: "50",
+            total: 20,
+            lastProfit: "50",
+            buildings: [{
+                name: "building1",
+                amount: 3
+            }, {
+                name: "building2",
+                amount: 5
+            }],
+            stock_shares: [{
+                name: "stock1",
+                amount: 3,
+                Annoymous: "12%"
+            }, {
+                name: "stock",
+                amount: 10,
+                Annoymous: "20%"
+            }]
+        }, {
+            company_id: "2",
+            company_name: "johannaTrashPeople",
+            year: "2001",
+            price: "50",
+            total: "20",
+            lastProfit: "70",
+            buildings: [{
+                name: "building1",
+                amount: 3,
+            }, {
+                name: "building2",
+                amount: 5
+            }],
+            stock_shares: [{
+                name: "stock1",
+                amount: 3,
+                Annoymous: "92%"
+            }, {
+                name: "stock",
+                amount: 10,
+                Annoymous: "82%"
+            }]
+        }];
+
+        var html = '';
+
         var randomScalingFactor = function () {
             return Math.round(Math.random() * 100);
         };
@@ -18,74 +68,121 @@
             return "rgb(" + r + ',' + g + ',' + b + ")";
         }
 
-        var data = [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()];
-        var backgroundColor = [];
-        var name = ['red', 'blue']
-        var labels = [];
-        var sum = 0;
-        // sum直接等于总股数
-        // 下面这个循环就先用着到时候删掉
-        for (var i = 0; i < data.length; i++) {
-            sum += data[i];
-        }
-        //
-        for (var i = 0; i < data.length; i++) {
-            backgroundColor.push(randomColor());
-        }
-
-        for (var i = 0; i < name.length; i++) {
-            labels.push(name[i] + "(" + ((data[i] / sum) * 100).toFixed(2) + "%)");
-        }
-
-        function information() {
-            $.ajax({
-                url: "wtfFrontEndIsShit.php",
-                dataType: "json",
-                type: "GET",
-                success: function (msg) {
-                    for (var i = 0; i < msg.length; i++) {
-                        for (var j = 0; j < msg[i].length; j++) {
-                            data[j] = msg[i][j];
-                            name[j] = msg[i][j];
-                        }
-                    }
-                },
-                error: function () {
-
-                }
-            })
-        }
-
-        $(document).ready(function () {
-            setTimeout("information()", 5000);
-        });
-
-        var config = {
-            type: 'pie',
-            data: {
-                datasets: [{
-                    data: data,
-                    backgroundColor: backgroundColor,
-                    label: 'Dataset 1'
-                }],
-                labels: labels
-            },
-            options: {
-                responsive: true
-            }
-        };
-
-        window.onload = function () {
-            var ctx = document.getElementById("chart-area").getContext("2d");
-            window.myPie = new Chart(ctx, config);
-        };
-
-
         function createDom() {
             $.each(receivedInfo, function () {
+                var data = [];
+                var backgroundColor = [];
+                var labels = [];
+                var label = this.company_name;
+
+                for (var i=0;i<this.stock_shares.length;i++) {
+                    data.push(this.stock_shares[i].amount);
+                    backgroundColor.push(randomColor());
+                    labels.push(this.stock_shares[i].name + "(" + ((this.stock_shares[i].amount / this.total) * 100).toFixed(2) + "%)");
+                }
+
+                var config = {
+                    type: 'pie',
+                    data: {
+                        datasets: [{
+                            data: data,
+                            backgroundColor: backgroundColor,
+                            label: label
+                        }],
+                        labels: labels
+                    },
+                    options: {
+                        responsive: true
+                    }
+                };
+
+                html += '<div class="company mdui-col-md-12"> <div class="mdui-card"> <div class="mdui-card-primary">';
+                html += '<div class="mdui-card-primary-title">'+ this.company_name + '</div> </div> <div class="mdui-card-content">';
+                html += '<div id="canvas-holder"> <canvas id="' + this.company_id + '"/> </div> <ul class="mdui-list">';
+                html += '<li class="mdui-list-item mdui-ripple">总股数：' + this.total + '</li>';
+                html += '<li class="mdui-list-item mdui-ripple">股价：' + this.price + '</li>';
+                html += '<li class="mdui-list-item mdui-ripple">去年的利润：' + this.lastProfit + '</li>';
+                html += '<li class="mdui-list-item mdui-ripple">公司持有的建筑：<ul>';
+                for (var i=0;i<this.buildings.length;i++) {
+                    html += '<li>'+ this.buildings[i].name + ' X ' + this.buildings[i].amount + '</li>';
+                }
+                html += '</ul><li class="mdui-list-item mdui-ripple">分红率：<ul>';
+                for (var i=0;i<this.stock_shares.length;i++) {
+                    html += '<li>'+ this.stock_shares[i].name + ': ' + this.stock_shares[i].Annoymous + '</li>';
+                }
+                html += '</ul></li> </ul> </div> </div> </div>';
+
+                var ctx = document.getElementById(this.company_id).getContext("2d");
+                window.myPie = new Chart(ctx, config);
             });
             $('#table').html(html);
         };
+
+
+        function information() {
+            // $.ajax({
+            //     url: "wtfFrontEndIsShit.php",
+            //     dataType: "json",
+            //     type: "GET",
+            //     success: function (msg) {
+            //         receivedInfo = msg;
+            //     },
+            //     error: function () {
+            //         alert("qbddbq");
+            //     }
+            // })
+            html = '';
+            createDom();
+        };
+
+        $(document).ready(function () {
+            setTimeout("information()", 500);
+        });
+
+        // var data = [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()];
+        // var backgroundColor = [];
+        // var name = ['red', 'blue']
+        // var labels = [];
+        // var sum = 0;
+        // // sum直接等于总股数
+        // // 下面这个循环就先用着到时候删掉
+        // for (var i = 0; i < data.length; i++) {
+        //     sum += data[i];
+        // }
+        // //
+        // for (var i = 0; i < data.length; i++) {
+        //     backgroundColor.push(randomColor());
+        // }
+        //
+        // for (var i = 0; i < name.length; i++) {
+        //     labels.push(name[i] + "(" + ((data[i] / sum) * 100).toFixed(2) + "%)");
+        // }
+        //
+        //
+        // $(document).ready(function () {
+        //     setTimeout("information()", 5000);
+        // });
+        //
+        // var config = {
+        //     type: 'pie',
+        //     data: {
+        //         datasets: [{
+        //             data: data,
+        //             backgroundColor: backgroundColor,
+        //             label: 'Dataset 1'
+        //         }],
+        //         labels: labels
+        //     },
+        //     options: {
+        //         responsive: true
+        //     }
+        // };
+        //
+        // window.onload = function () {
+        //     var ctx = document.getElementById("chart-area").getContext("2d");
+        //     window.myPie = new Chart(ctx, config);
+        // };
+
 
     </script>
 @endsection
@@ -131,7 +228,7 @@
                 Company Report
             </div>
         </div>
-        <div class="mdui-col-md-12">
+        <div id="table" class="mdui-col-md-12">
             <div class="company mdui-col-md-12">
                 <div class="mdui-card">
                     <div class="mdui-card-primary">
@@ -148,27 +245,6 @@
                             <li class="mdui-list-item mdui-ripple">公司持有的建筑：</li>
                             <li class="mdui-list-item mdui-ripple">分红率：</li>
                         </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="company mdui-col-md-6">
-                <div class="mdui-card">
-                    <div class="mdui-card-primary">
-                        <div class="mdui-card-primary-title">CompanyName2</div>
-                    </div>
-                </div>
-            </div>
-            <div class="company mdui-col-md-6">
-                <div class="mdui-card">
-                    <div class="mdui-card-primary">
-                        <div class="mdui-card-primary-title">CompanyName3</div>
-                    </div>
-                </div>
-            </div>
-            <div class="company mdui-col-md-6">
-                <div class="mdui-card">
-                    <div class="mdui-card-primary">
-                        <div class="mdui-card-primary-title">CompanyName4</div>
                     </div>
                 </div>
             </div>
