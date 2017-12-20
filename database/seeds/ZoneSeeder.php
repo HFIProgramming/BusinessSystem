0209aa75d5646ba3a2caa4f7030d7d0fcdb42e0a
ZihongLin <1046298030@qq.com>
2017-12-20 16:47:10 +0800

    again some seeder change

--- database/seeds/ZoneSeeder.php
+++ database/seeds/ZoneSeeder.php
@@ -16,11 +16,18 @@ class ZoneSeeder extends Seeder
         //
         for ($i = 1; $i <= 3; $i++) {
             $user = User::create([
-                'name' => 'Zone'.$i,
-                'email' => 'Zone'.$i.'@finance.club',
+                'name' => 'Zone' . $i,
+                'email' => 'Zone' . $i . '@finance.club',
                 'password' => bcrypt('zonezonezone'),
                 'type' => 3
             ]);
+            foreach (Resources::all() as $resource) {
+                $user->resources()->create([
+                    'resource_id' => $resource->id,
+                    'user_id' => $user->id,
+                    'amount' => 0,
+                ]);
+            }
             Zone::create([
                 'user_id' => $user->id
             ]);
