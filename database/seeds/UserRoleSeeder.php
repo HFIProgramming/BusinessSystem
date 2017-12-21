3ca5191ec0b97fd516750bbacc57450aeafd3bf0
ZihongLin <1046298030@qq.com>
2017-12-21 12:17:48 +0800

    fixfix

--- database/seeds/UserRoleSeeder.php
+++ database/seeds/UserRoleSeeder.php
@@ -25,9 +25,16 @@ class UserRoleSeeder extends Seeder
 			'user_transaction_type' => [0, 1, 2, 3, 4, 5, 6],
 		];
 
+        $default3 = [
+            'user_type'             => 0,
+            'resource_type'         => [0, 1, 2, 3, 4, 5, 6],
+            'user_transaction_type' => [0, 1, 2, 3, 4, 5, 6],
+        ];
+
 		//@TODO This is to be changed.
 
 		\App\UserTransactionRule::query()->create($default1);
 		\App\UserTransactionRule::query()->create($default2);
+        \App\UserTransactionRule::query()->create($default3);
 	}
 }
