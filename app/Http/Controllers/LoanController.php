29150b0efe51ed5041d44a2bacfbe4f85c4727e2
ZihongLin <1046298030@qq.com>
2017-12-22 23:12:25 +0800

    Loan user limit

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -38,6 +38,10 @@ class LoanController extends Controller
         {
             return view('errors.custom')->with('message', '不支持给冥界放款哦 请检查对方ID是否正确');//@TODO 这页面丑死了 有空换一个吧
         }
+        if(!($creditor->type == 2 && $debtor->type == 2))
+        {
+            return view('errors.custom')->with('message', '你们之间不允许接待操作');
+        }
         if($debtor->resources()->resid(1)->first()->amount < $amount)
         {
             return view('errors.custom')->with('message', '不够钱还是先别放款了吧');
