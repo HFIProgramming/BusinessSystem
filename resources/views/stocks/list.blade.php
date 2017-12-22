020016bd13b1b44e1b7ef2b36d47f79aa6022294
ZihongLin <1046298030@qq.com>
2017-12-23 05:05:39 +0800

    kill comments

--- resources/views/stocks/list.blade.php
+++ resources/views/stocks/list.blade.php
@@ -5,8 +5,7 @@
 @endsection
 {{-- @TODO labels and frontend shit fix--}}
 @section('script')
-    <script src="js/Chart.bundle.js"></script>
-    <script src="js/utils.js"></script>
+    <script src="{{ asset('js/Chart.bundle.js') }}"></script>
     <script>
 
         var html = '';
@@ -31,28 +30,6 @@
             return "rgb(" + r + ',' + g + ',' + b + ")";
         }
 
-        //                var receivedInfo = [{
-        //                        "id": 001,
-        //                        "current_price": 10,
-        //                        "all_prices": [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()],
-        //                        "company_name": "test1",
-        //                        "total": 500,
-        //                        "dividend": 20,
-        ////                        "hand_up": 0,
-        //                        "sell_remain": 5,
-        //                        "buy_remain": 10
-        //                    }, {
-        //                        "id": 002,
-        //                        "current_price": 100,
-        //                        "all_prices": [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()],
-        //                        "company_name": "test2",
-        //                        "total": 5000,
-        //                        "dividend": 200,
-        ////                        "hand_up": 20,
-        //                        "sell_remain": 50,
-        //                        "buy_remain": 100
-        //                    }]
-        //                ;
 
         function information() {
             $.ajax({
@@ -111,6 +88,7 @@
                             }
                         }
                     });
+                    window.myLine.update();
 
                     createDom();
                 },
@@ -134,107 +112,16 @@
                 $("#" + this.id).children('.mdui-panel-item').children('.mdui-panel-item-body').children(".b4").text("买盘剩余：" + this.buy_remain);
                 $("#" + this.id).children('.mdui-panel-item').children('.mdui-panel-item-body').children(".b7").text("风险评估：" + this.risk_evaluation);
 
-
-                {{--html += '<tr> <td class="mdui-panel" mdui-panel> <div class="mdui-panel-item mdui-panel-item-open"> <div class="mdui-panel-item-header"> <div class="mdui-panel-item-title">';--}}
-                {{--html += this.company_name + '</div>';--}}
-                {{--html += '<div class="mdui-panel-item-summary">Current Price: ' + this.current_price + '</div>';--}}
-                {{--//                html += '<div class="mdui-panel-item-summary">Now you have: ' + this.hand_up + '</div>';--}}
-                {{--html += '<i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i> </div>';--}}
-                {{--html += '<div class="mdui-panel-item-body"> <p>total: ' + this.total + '</p>';--}}
-                {{--html += '<p>dividend: ' + this.dividend + '</p>';--}}
-                {{--html += '<p>sell remain: ' + this.sell_remain + '</p>';--}}
-                {{--html += '<p>buy remain: ' + this.buy_remain + '</p>';--}}
-                {{--html += '</div> </div> </td>'--}}
-                {{--html2 = '<td> <form action="{{ route('buyStock') }}" method="post"> {{ csrf_field() }}';--}}
-                {{--html2 += '<input type="hidden" name="stock_id" value=' + this.id + '>';--}}
-                {{--html2 += '<div class="mdui-textfield"> <input class="mdui-textfield-input" type="text" name="amount" placeholder="$"> </div> <button type="submit" class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">Buy </button> </form> </td>';--}}
-                {{--html2 += '<td> <form action="{{ route('sellStock') }}" method="post"> {{ csrf_field() }}';--}}
-                {{--html2 += '<input type="hidden" name="stock_id" value=' + this.id + '>';--}}
-                {{--html2 += '<div class="mdui-textfield"> <input class="mdui-textfield-input" type="text" name="amount" placeholder="$"> </div> <button type="submit" class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">Sell </button> </form> </td> </tr>';--}}
-
             });
         }
 
-        //
-        //
-        // function arrow() {
-        //     if ($(this).parents(".mdui-panel-item").hasClass("mdui-panel-item-open") === false) {
-        //         $(this).parents(".mdui-panel-item").addClass("mdui-panel-item-open");
-        //     } else {
-        //         $(this).parents(".mdui-panel-item").removeClass("mdui-panel-item-open");
-        //     }
-        // }
 
         $(document).ready(function () {
 
             setInterval("information()", 5000);
             // createDom();
-//            $(html).insertAfter("#table");
-//            document.getElementById("table").innerHTML = html;
-        });
-
-
-        //        var datasets = [{
-        //            lineTension: 0,
-        //            label: "My First dataset",
-        //            borderColor: window.chartColors.red,
-        //            backgroundColor: window.chartColors.red,
-        //            fill: false,
-        //            data: data1,
-        //            yAxisID: "y-axis",
-        //        }, {
-        //            lineTension: 0,
-        //            label: "My Second dataset",
-        //            borderColor: window.chartColors.blue,
-        //            backgroundColor: window.chartColors.blue,
-        //            fill: false,
-        //            data: data2,
-        //            yAxisID: "y-axis"
-        //        }, {
-        //            lineTension: 0,
-        //            label: "My Third dataset",
-        //            borderColor: window.chartColors.blue,
-        //            backgroundColor: window.chartColors.blue,
-        //            fill: false,
-        //            data: data3,
-        //            yAxisID: "y-axis"
-        //        }];
-
-        // var lineChartData = {
-        //     labels: labels,
-        //     datasets: datasets
-        // };
-        //
-        // window.onload = function () {
-        //     var ctx = document.getElementById("stock").getContext("2d");
-        //     window.myLine = Chart.Line(ctx, {
-        //         data: lineChartData,
-        //         options: {
-        //             responsive: true,
-        //             hoverMode: 'index',
-        //             stacked: false,
-        //             title: {
-        //                 display: false,
-        //                 text: 'Chart.js Line Chart - Multi Axis'
-        //             },
-        //             scales: {
-        //                 yAxes: [{
-        //                     type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-        //                     display: true,
-        //                     position: "left",
-        //                     id: "y-axis",
-        //                     // grid line settings
-        //                     gridLines: {
-        //                         drawOnChartArea: true, // only want the grid lines for one axis to show up
-        //                     },
-        //                 }],
-        //             }
-        //         }
-        //     });
-        // };
 
     </script>
-    <script src="{{ asset('js/Chart.bundle.js') }}"></script>
 
 @endsection
 
@@ -331,7 +218,7 @@
                                 <form action="{{ route('buyStock') }}" method="post"> {{ csrf_field() }}
                                     <input type="hidden" name="stock_id" value={{$stock->id}}>
                                     <div class="mdui-textfield">
-                                        <input class="mdui-textfield-input" type="text" name="amount" placeholder="$">
+                                        <input class="mdui-textfield-input" type="text" name="amount" placeholder="#">
                                     </div>
                                     <button type="submit"
                                             class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">
@@ -344,7 +231,7 @@
                                 <form action="{{ route('sellStock') }}" method="post"> {{ csrf_field() }}
                                     <input type="hidden" name="stock_id" value={{$stock->id}}>
                                     <div class="mdui-textfield">
-                                        <input class="mdui-textfield-input" type="text" name="amount" placeholder="$">
+                                        <input class="mdui-textfield-input" type="text" name="amount" placeholder="#">
                                     </div>
                                     <button type="submit"
                                             class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">
