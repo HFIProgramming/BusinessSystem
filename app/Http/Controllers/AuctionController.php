e9b6d450a6cf44bccd6d30f4b368ce27432a82eb
ZihongLin <1046298030@qq.com>
2019-02-10 03:52:05 +0800

    unimportant work and frontend scaffholding

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -12,43 +12,51 @@ class AuctionController extends Controller
     //
     public function showBidForm(Request $request)
     {
-    	if(Config::KeyValue('auction_activated') == 1)
-    	{
-
-    	}
-    	else
-    	{
-    		return view('errors.custom')->with('message', '目前不能竞拍呢！');
-    	}
+        $user = $request->user();
+        $year = Config::KeyValue('current_round')->value;
+        if(Config::KeyValue('auction_activated') == 1)
+        {
+            if(empty($user->auctions()->where('year', $year)->first()))
+            {
+                return view('auction.createBid')->with('year', $year)->with('auction_amount', Config::KeyValue('auction_amount')->value);
+            }
+            return view('errors.custom')->with('message', '本财年已经提交过竞拍啦');
+        }
+        else
+        {
+            return view('errors.custom')->with('message', '目前不能竞拍呢！');
+        }
     }
 
     public function listBids(Request $request)
     {
-
+        $user = $request->user();
+        $bids = $user->auctions()->latest()->get();
+        return view('auction.list')->with('bids', $bids);
     }
 
     public function submitBid(Request $request)
     {
-    	$user = $request->user();
-    	$price = $request->price;
-    	if(empty(IntToVal('acceptable_bid_range', $price)))
-    	{
-    		//unacceptable price
-    		return view('errors.custom')->with('message', '竞拍价格不在可接受范围内');
-    	}
-    	if($user->resources()->resid(1)->first()->amount < $price)
-    	{
-    		//not enough money
-    		return view('errors.custom')->with('message', '竞价可别放卫星呀！您没有这么多钱！');
-    	}
-    	if(Config::KeyValue('auction_activated') == 0)
-    	{
-    		return view('errors.custom')->with('message', '目前不能竞拍呢！');
-    	}
-    	$user->auctions()->create([
-    		'price' => $price,
-    		'year' => Config::KeyValue('current_round')->value,
-    		'status' => 'submitted'
-    	]);
+        $user = $request->user();
+        $price = $request->price;
+        if(empty(IntToVal('acceptable_bid_range', $price)))
+        {
+            //unacceptable price
+            return view('errors.custom')->with('message', '竞拍价格不在可接受范围内');
+        }
+        if($user->resources()->resid(1)->first()->amount < $price)
+        {
+            //not enough money
+            return view('errors.custom')->with('message', '竞价可别放卫星呀！您没有这么多钱！');
+        }
+        if(Config::KeyValue('auction_activated') == 0)
+        {
+            return view('errors.custom')->with('message', '目前不能竞拍呢！');
+        }
+        $user->auctions()->create([
+            'price' => $price,
+            'year' => Config::KeyValue('current_round')->value,
+            'status' => 'submitted'
+        ]);
     }
 }
