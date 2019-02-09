b401c7618a7519fa4e919cc07682bd29e7cc9115
ZihongLin <1046298030@qq.com>
2019-02-09 23:56:14 +0800

    auction and acquisition setup (models, controllers, Eloquent relations)

---
+++ app/Auction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Auction extends Model
+{
+    //
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+}
---
+++ database/migrations/2019_02_09_233729_create_auctions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuctionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auctions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id').
+            $table->integer('year');
+            $table->integer('price');
+            $table->string('status'); //submitted, unsuccessful, successful
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auctions');
+    }
+}
