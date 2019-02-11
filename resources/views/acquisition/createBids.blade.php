9d6c020da2ccc3fd7546e448387cca9a6141869f
johanna sun <sunyx.johanna2016@outlook.com>
2019-02-11 19:13:57 +0800

    robot bidding finish

---
+++ resources/views/acquisition/createBids.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.base')
+
+@section('title')
+    robots bidding
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+    var arry = $("#robotBidding").serializeArray();
+
+    $.ajax({
+    url: "/Login/Index",
+    data: { jsondata: JSON.stringify(arry) },
+    type:"post",
+    success: function () {
+    alert("success");
+    }
+    });
+
+    </script>
+@endsection
+
+@section('stylesheet')
+
+@endsection
+
+@section('body')
+    <div class="mdui-container doc-container">
+        <div class="mdui-row">
+            <div class="mdui-col-xs-12">
+                <div class="mdui-card">
+                    <div class="mdui-card-header">
+                        <div class="mdui-typo-display-2 mdui-text-center mdui-text-color-theme">
+                            收购机器人的投标
+                        </div>
+                    </div>
+                    <div class="mdui-card-header-subtitle adjust_card_subtitle">
+                        <div class="mdui-text-center">
+                            便捷金融生活从此开启
+                        </div>
+                    </div>
+
+                    <div class="mdui-card-content mdui-typo">
+                        <table class="mdui-table">
+                            <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>数量</th>
+                                <th>出价</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <form id="robotBidding" method="post" action="{{route('newBidding')}}">
+                                {{ csrf_field() }}
+                                @foreach($acquisition_items_and_amount as $item => $amount)
+                                    <tr>
+                                        <td>{{$item}}</td>
+                                        <input type="hidden" id="itemId" name="itemId" value="{{$item}}">
+                                        <td>
+                                            <div class="mdui-textfield mdui-textfield-floating-label">
+                                                <i class="mdui-icon material-icons adjust_mdui_icon">shopping_basket</i>
+                                                <label class="mdui-textfield-label">主席团准备购入{{$item => $amount}}个</label>
+                                                <input class="mdui-textfield-input" id="amount" name="amount" type="number"/>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="mdui-textfield mdui-textfield-floating-label">
+                                                <i class="mdui-icon material-icons adjust_mdui_icon">shopping_basket</i>
+                                                <label class="mdui-textfield-label">我的出价</label>
+                                                <input class="mdui-textfield-input" id="price" name="price" type="number"/>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                <button data-no-instant class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme mdui-center">
+                                    提交
+                                </button>
+                            </form>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <!--row-->
+        </div>
+    </div>
+@endsection
