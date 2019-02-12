4a774815de57487d09f71b85a13de1dd8fbca6ad
johanna sun <sunyx.johanna2016@outlook.com>
2019-02-13 01:20:24 +0800

    competitors frontend fixed

--- resources/views/resources/build.blade.php
+++ resources/views/resources/build.blade.php
@@ -23,11 +23,7 @@
                     </div>
                 </div>
 
-                <div class="mdui-card-header-subtitle adjust_card_subtitle">
-                    <div class="mdui-text-center">
-                        便捷金融生活从此开启
-                    </div>
-                </div>
+
                 <div class="mdui-card-content mdui-typo">
                     <form method="post" action="{{route('build')}}">
                         {{ csrf_field() }}
